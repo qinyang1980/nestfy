0fe3d4f74b040552e5c07dba41bd7dca68f68da0
Andy Qin <qinyang_1980@qq.com>
2018-11-15 19:02:25 +0800

    refine

--- src/app/modules/photo/photo.controller.ts
+++ src/app/modules/photo/photo.controller.ts
@@ -13,80 +13,77 @@ import { PhotoService } from './photo.service';
 export class PhotoController {
   constructor(private readonly _photoService: PhotoService) {}
 
-  // @Post()
-  // public async create(@Body() createPhotoDto: CreatePhotoDto): Promise<Photo> {
-  //   return this._photoService.create(createPhotoDto as Photo);
-  // }
+  @Post()
+  public async create(@Body() createPhotoDto: CreatePhotoDto): Promise<Photo> {
+    return this._photoService.create(createPhotoDto as Photo);
+  }
 
   @Get()
   public async findAllAndCount(): Promise<any> {
     return this._photoService.findAllAndCount();
   }
 
-  // @Get('findAll')
-  // public async findAll(): Promise<any> {
-  //   return this._photoService.findAll();
-  // }
+  @Get('findAll')
+  public async findAll(): Promise<any> {
+    return this._photoService.findAll();
+  }
 
-  // @Get(':id')
-  // @Auth(false)
-  // public findOne(
-  //   @Param('id', new ParseIntPipe())
-  //   id: number
-  // ): Promise<Photo> {
-  //   return this._photoService.findOneById(id);
-  // }
+  @Get(':id')
+  @Auth(false)
+  public findOne(
+    @Param('id', new ParseIntPipe())
+    id: number
+  ): Promise<Photo> {
+    return this._photoService.findOneById(id);
+  }
 
-  // @Put(':id')
-  // public async modify(
-  //   @Param('id', new ParseIntPipe())
-  //   id: number,
-  //   @Body() photo: CreatePhotoDto
-  // ): Promise<Photo> {
-  //   return this._photoService.modify(id, photo as Photo);
-  // }
+  @Put(':id')
+  public async modify(
+    @Param('id', new ParseIntPipe())
+    id: number,
+    @Body() photo: CreatePhotoDto
+  ): Promise<Photo> {
+    return this._photoService.modify(id, photo as Photo);
+  }
 
-  // @Delete(':id')
-  // public async remove(
-  //   @Param('id', new ParseIntPipe())
-  //   id: number
-  // ): Promise<any> {
-  //   return this._photoService.remove(id);
-  // }
+  @Delete(':id')
+  public async remove(@Param('id', new ParseIntPipe()) id: number): Promise<any> {
+    return this._photoService.remove(id);
+  }
 
-  // // 异常测试
-  // @Get('exception/testing')
-  // public async exceptionTesting(): Promise<any> {
-  //   throw new Error('test');
-  // }
+  // 异常测试
+  @Get('exception/testing')
+  public async exceptionTesting(): Promise<any> {
+    throw new Error('test');
+  }
 
-  // @Get('exception/normal-error')
-  // public async testNormalError(): Promise<any> {
-  //   throw new Error('test');
-  // }
+  @Get('exception/normal-error')
+  public async testNormalError(): Promise<any> {
+    throw new Error('test');
+  }
 
-  // @Get('exception/special-error')
-  // public async testForbiddenError(): Promise<any> {
-  //   throw new ForbiddenException('forbidden');
-  // }
+  @Get('exception/special-error')
+  public async testForbiddenError(): Promise<any> {
+    throw new ForbiddenException('forbidden');
+  }
 
-  // @Get('exception/config-error')
-  // public async testConfigError(): Promise<any> {
-  //   throw config.error(-1300);
-  // }
+  @Get('exception/config-error')
+  public async testConfigError(): Promise<any> {
+    throw config.error(-1300);
+  }
 
-  // @Get('exception/programming-error')
-  // public async testProgrammingError(): Promise<any> {
-  //   return (this as any).func2222();
-  // }
+  @Get('exception/programming-error')
+  public async testProgrammingError(): Promise<any> {
+    return (this as any).func2222();
+  }
 
-  // @Get('exception/database-error')
-  // public async testDatabaseError(): Promise<any> {
-  //   //return this._photoService.dbExceptionTest();
-  // }
+  @Get('exception/database-error')
+  public async testDatabaseError(): Promise<any> {
+    return this._photoService.dbExceptionTest();
+  }
 
-  // @Post('exception/validation-error')
-  // public async testValidationError(@Body() createPhotoDto: CreatePhotoDto): Promise<any> {
-  //   return 3;
-  // }
+  @Post('exception/validation-error')
+  public async testValidationError(@Body() createPhotoDto: CreatePhotoDto): Promise<any> {
+    return 3;
+  }
 }
--- src/app/modules/photo/photo.service.ts
+++ src/app/modules/photo/photo.service.ts
@@ -17,52 +17,52 @@ export class PhotoService {
     return this._photoRepository.findAndCount();
   }
 
-  // /**
-  //  * 获取所有photos记录
-  //  */
-  // public findAll(): Promise<Photo[]> {
-  //   return this._photoRepository.find();
-  // }
+  /**
+   * 获取所有photos记录
+   */
+  public findAll(): Promise<Photo[]> {
+    return this._photoRepository.find();
+  }
 
-  // /**
-  //  * 根据id获取单个photo
-  //  */
-  // public async findOneById(id: number): Promise<Photo> {
-  //   return this._photoRepository.findOne(id);
-  // }
+  /**
+   * 根据id获取单个photo
+   */
+  public async findOneById(id: number): Promise<Photo> {
+    return this._photoRepository.findOne(id);
+  }
 
-  // /**
-  //  * 根据photo对象创建一个新的记录
-  //  */
-  // public async create(photo: Photo): Promise<Photo> {
-  //   return this._photoRepository.save(photo);
-  // }
+  /**
+   * 根据photo对象创建一个新的记录
+   */
+  public async create(photo: Photo): Promise<Photo> {
+    return this._photoRepository.save(photo);
+  }
 
-  // /**
-  //  * 根据id，修改某条记录
-  //  */
-  // public async modify(id: number, photo: Photo): Promise<Photo> {
-  //   photo.id = id;
-  //   return this._photoRepository.save(photo);
-  // }
+  /**
+   * 根据id，修改某条记录
+   */
+  public async modify(id: number, photo: Photo): Promise<Photo> {
+    photo.id = id;
+    return this._photoRepository.save(photo);
+  }
 
-  // /**
-  //  * 删除某条记录
-  //  */
-  // public async remove(id: number): Promise<any> {
-  //   const photo: Photo = {} as Photo;
-  //   photo.id = id;
-  //   return this._photoRepository.remove(photo);
-  // }
+  /**
+   * 删除某条记录
+   */
+  public async remove(id: number): Promise<any> {
+    const photo: Photo = {} as Photo;
+    photo.id = id;
+    return this._photoRepository.remove(photo);
+  }
 
-  // /**
-  //  * 数据库异常测试
-  //  */
-  // public async dbExceptionTest(): Promise<any> {
-  //   const id: number = 3;
-  //   return this._photoRepository
-  //     .createQueryBuilder('photo')
-  //     .where('photo.id2 = :id', { id })
-  //     .getMany();
-  // }
+  /**
+   * 数据库异常测试
+   */
+  public async dbExceptionTest(): Promise<any> {
+    const id: number = 3;
+    return this._photoRepository
+      .createQueryBuilder('photo')
+      .where('photo.id2 = :id', { id })
+      .getMany();
+  }
 }
