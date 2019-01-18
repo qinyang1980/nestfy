f488fc65e9433f063d0c8e4770a13025d55b1f71
Andy Qin <qinyang_1980@qq.com>
2019-01-18 14:18:06 +0800

    refine

--- src/lib/utils/app.util.ts
+++ src/lib/utils/app.util.ts
@@ -15,71 +15,67 @@ export class AppUtil {
   public static async bootstrap(appModule: any): Promise<void> {
     const app = await NestFactory.create(appModule);
 
-    try {
-      if (config.request.bodyParser.enable) {
-        app.use(bodyParser.json({ limit: config.request.bodyParser.limit }));
-        app.use(bodyParser.urlencoded({ limit: config.request.bodyParser.limit, extended: false }));
-      }
-
-      // 跨域支持
-      if (config.request.cors.enable) {
-        app.enableCors();
-      }
-
-      // 是否打印请求
-      if (config.request.traceRequestDuration) {
-        app.use(requestLogMiddleware);
-      }
-
-      // 日志配置
-      if (config.logging.enable) {
-        configure(config.logging.configuration);
-      }
-
-      // 校验Request
-      if (config.request.validation.enable) {
-        app.useGlobalPipes(
-          new ValidationPipe({ skipMissingProperties: config.request.validation.skipMissingProperties }),
-        );
-      }
-
-      // 路由前缀
-      if (config.app.apiPrefix.enable) {
-        app.setGlobalPrefix(config.app.apiPrefix.prefix);
-      }
-
-      // token校验
-      if (config.request.auth.enable) {
-        app.useGlobalGuards(new AuthGuard(new Reflector()));
-      }
-
-      // 统一错误处理
-      app.useGlobalFilters(new ErrorFilter());
-
-      // 统一response格式化处理
-      app.useGlobalInterceptors(new TransformInterceptor());
-
-      const schemas =
-        config.swagger.schemas === 'http' || config.swagger.schemas === 'https' ? config.swagger.schemas : 'http';
-
-      if (config.swagger.enable) {
-        const options = new DocumentBuilder()
-          .setTitle(config.swagger.title)
-          .setBasePath(config.app.apiPrefix.prefix)
-          .setDescription(config.swagger.description)
-          .setVersion(config.swagger.version)
-          .setSchemes(schemas)
-          .setContactEmail(config.swagger.email)
-          .build();
-        const document = SwaggerModule.createDocument(app, options);
-        SwaggerModule.setup(config.swagger.path, app, document);
-      }
-
-      app.listen(config.app.port, () => {
-        logger.info(config.app.setUpMsg);
-      });
-    } catch (err) {
-      logger.error(err);
+    if (config.request.bodyParser.enable) {
+      app.use(bodyParser.json({ limit: config.request.bodyParser.limit }));
+      app.use(bodyParser.urlencoded({ limit: config.request.bodyParser.limit, extended: false }));
     }
+
+    // 跨域支持
+    if (config.request.cors.enable) {
+      app.enableCors();
+    }
+
+    // 是否打印请求
+    if (config.request.traceRequestDuration) {
+      app.use(requestLogMiddleware);
+    }
+
+    // 日志配置
+    if (config.logging.enable) {
+      configure(config.logging.configuration);
+    }
+
+    // 校验Request
+    if (config.request.validation.enable) {
+      app.useGlobalPipes(
+        new ValidationPipe({ skipMissingProperties: config.request.validation.skipMissingProperties }),
+      );
+    }
+
+    // 路由前缀
+    if (config.app.apiPrefix.enable) {
+      app.setGlobalPrefix(config.app.apiPrefix.prefix);
+    }
+
+    // token校验
+    if (config.request.auth.enable) {
+      app.useGlobalGuards(new AuthGuard(new Reflector()));
+    }
+
+    // 统一错误处理
+    app.useGlobalFilters(new ErrorFilter());
+
+    // 统一response格式化处理
+    app.useGlobalInterceptors(new TransformInterceptor());
+
+    const schemas =
+      config.swagger.schemas === 'http' || config.swagger.schemas === 'https' ? config.swagger.schemas : 'http';
+
+    if (config.swagger.enable) {
+      const options = new DocumentBuilder()
+        .setTitle(config.swagger.title)
+        .setBasePath(config.app.apiPrefix.prefix)
+        .setDescription(config.swagger.description)
+        .setVersion(config.swagger.version)
+        .setSchemes(schemas)
+        .setContactEmail(config.swagger.email)
+        .build();
+      const document = SwaggerModule.createDocument(app, options);
+      SwaggerModule.setup(config.swagger.path, app, document);
+    }
+
+    app.listen(config.app.port, () => {
+      logger.info(config.app.setUpMsg);
+    });
   }
 }
